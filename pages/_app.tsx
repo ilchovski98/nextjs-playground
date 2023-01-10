7a717a15417455ec3cac63e60f3645821a2e50ba
ilchovski98 <ilchovski98@gmail.com>
2023-01-10 20:20:57 +0200

    Add connect & execute functions by using web3-react

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import {Web3ReactProvider} from '@web3-react/core';
+import {Web3Provider} from '@ethersproject/providers';
+
+const getLibrary = (provider) => {
+  return new Web3Provider(provider);
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Web3ReactProvider>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,353 +1,41 @@
+import {ethers} from 'ethers';
+import {useWeb3React} from '@web3-react/core';
+import {InjectedConnector} from '@web3-react/injected-connector'
+
 import {server} from '../config';
 import CommentList from '../components/CommentList';
-import {ethers} from 'ethers';
-import {useState} from 'react';
+import {abi} from '../constants/raffle';
+
+const injected = new InjectedConnector();
 
 export default function Home({comments}) {
-  const [isConnected, setIsConnected] = useState(false);
-  const [signer, setSigner] = useState();
+  const { activate, active, library: provider } = useWeb3React();
 
   async function connect() {
-    if (typeof window.ethereum !== 'undefined') {
-      try {
-        await window.ethereum.request({method: 'eth_requestAccounts'});
-        setIsConnected(true);
-        let connectedProvider = new ethers.providers.Web3Provider(window.ethereum);
-        setSigner(connectedProvider.getSigner());
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      setIsConnected(false);
+    try {
+      await activate(injected);
+    } catch (error) {
+      console.log(error);
     }
   }
 
   async function execute() {
-    if (typeof window.ethereum !== 'undefined') {
+    if (active) {
+      const signer = provider.getSigner();
       const contractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
-      const abi = [
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "vrfCoordinatorV2",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "entranceFee",
-              "type": "uint256"
-            },
-            {
-              "internalType": "bytes32",
-              "name": "gasLane",
-              "type": "bytes32"
-            },
-            {
-              "internalType": "uint64",
-              "name": "subscriptionId",
-              "type": "uint64"
-            },
-            {
-              "internalType": "uint32",
-              "name": "callbackGasLimit",
-              "type": "uint32"
-            },
-            {
-              "internalType": "uint256",
-              "name": "interval",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "nonpayable",
-          "type": "constructor"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "have",
-              "type": "address"
-            },
-            {
-              "internalType": "address",
-              "name": "want",
-              "type": "address"
-            }
-          ],
-          "name": "OnlyCoordinatorCanFulfill",
-          "type": "error"
-        },
-        {
-          "inputs": [],
-          "name": "Raffle__RaffleNotOpen",
-          "type": "error"
-        },
-        {
-          "inputs": [],
-          "name": "Raffle__SendMoreToEnterRaffle",
-          "type": "error"
-        },
-        {
-          "inputs": [],
-          "name": "Raffle__TransferFailed",
-          "type": "error"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "currentBalance",
-              "type": "uint256"
-            },
-            {
-              "internalType": "uint256",
-              "name": "numPlayers",
-              "type": "uint256"
-            },
-            {
-              "internalType": "uint256",
-              "name": "raffleState",
-              "type": "uint256"
-            }
-          ],
-          "name": "Raffle__UpkeepNotNeeded",
-          "type": "error"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "player",
-              "type": "address"
-            }
-          ],
-          "name": "RaffleEnter",
-          "type": "event"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": true,
-              "internalType": "uint256",
-              "name": "requestId",
-              "type": "uint256"
-            }
-          ],
-          "name": "RequestedRaffleWinner",
-          "type": "event"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "player",
-              "type": "address"
-            }
-          ],
-          "name": "WinnerPicked",
-          "type": "event"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "bytes",
-              "name": "",
-              "type": "bytes"
-            }
-          ],
-          "name": "checkUpkeep",
-          "outputs": [
-            {
-              "internalType": "bool",
-              "name": "upkeepNeeded",
-              "type": "bool"
-            },
-            {
-              "internalType": "bytes",
-              "name": "",
-              "type": "bytes"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "enterRaffle",
-          "outputs": [],
-          "stateMutability": "payable",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "getEntranceFee",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "getInterval",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "getLastTimeStamp",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "getNumWords",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "pure",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "getNumberOfPlayers",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "index",
-              "type": "uint256"
-            }
-          ],
-          "name": "getPlayer",
-          "outputs": [
-            {
-              "internalType": "address",
-              "name": "",
-              "type": "address"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "getRaffleState",
-          "outputs": [
-            {
-              "internalType": "enum Raffle.RaffleState",
-              "name": "",
-              "type": "uint8"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "getRecentWinner",
-          "outputs": [
-            {
-              "internalType": "address",
-              "name": "",
-              "type": "address"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "getRequestConfirmations",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "pure",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "bytes",
-              "name": "",
-              "type": "bytes"
-            }
-          ],
-          "name": "performUpkeep",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "requestId",
-              "type": "uint256"
-            },
-            {
-              "internalType": "uint256[]",
-              "name": "randomWords",
-              "type": "uint256[]"
-            }
-          ],
-          "name": "rawFulfillRandomWords",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        }
-      ];
       const contract = new ethers.Contract(contractAddress, abi, signer);
-      await contract.enterRaffle({value: ethers.utils.parseEther("10")});
+      try {
+        await contract.enterRaffle({value: ethers.utils.parseEther("10")});
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 
   return (
     <>
       {
-        isConnected ?
+        active ?
         <>
           <button>Connected!</button>
           <button onClick={execute}>Execute</button>
